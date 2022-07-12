880bed64dcf6dcf331db3438c348e3fe8cb008a5
Mashawabupr <marikanik1999@gmail.com>
2022-07-12 13:18:24 +0300

    added API integration and meaning of word

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import "./App.css";
 import Dictionary from "./Dictionary";
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-center">Dictionary</h1>
+    <div className="App text-center">
+      <h1>Dictionary</h1>
       <Dictionary />
     </div>
   );
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,42 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 export default function Dictionary() {
   let [input, setInput] = useState("");
+  let [info, setInfo] = useState({});
+  function showData(response) {
+    console.log(response.data[0].meanings[0].definitions[0].definition);
+    setInfo({
+      name: response.data[0].word,
+      partOfSpeach: response.data[0].meanings[0].partOfSpeech,
+      def: response.data[0].meanings[0].definitions[0].definition,
+    });
+  }
   function handleSubmit(event) {
     event.preventDefault();
-    alert(`Searching for ${input}`);
+    let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${input}`;
+    axios.get(url).then(showData);
   }
   function handleChange(event) {
     setInput(event.target.value);
   }
+
   return (
-    <form className="text-center m-5" onSubmit={handleSubmit}>
-      <input type="search" placeholder="Enter a word" onChange={handleChange} />
-    </form>
+    <div>
+      <form className="text-center m-5" onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Enter a word"
+          onChange={handleChange}
+        />
+      </form>
+      <div className="row justify-content-center">
+        <div className="info col-6 ">
+          <h4>{info.name}</h4>
+          <h4>{info.partOfSpeach}</h4>
+          <p>{info.def}</p>
+        </div>
+      </div>
+    </div>
   );
 }
